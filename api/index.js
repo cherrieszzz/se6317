c83ef995ccb50316f4c0565dfad2a9871c54e3d6
Gatsby <greenteacore@outlook.com>
2023-04-07 15:06:26 +0800

    en

---
+++ api/index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Signup route
+app.post('/api/signup', function (req, res) {
+  // Handle POST request here
+});
+
+// Update password route
+app.post('/api/updatepasswd', function (req, res) {
+  // Handle POST request here
+});
+
+// Get all blogs route
+app.get('/api/blogs', function (req, res) {
+  // Handle GET request here
+});
+
+// Get single blog route
+app.get('/api/blogs/:blogid', function (req, res) {
+  // Handle GET request here
+});
+
+// Delete blog route
+app.delete('/api/blogs/:blogid', function (req, res) {
+  // Handle DELETE request here
+});
+
+// Get user route
+app.get('/api/:userid', function (req, res) {
+  // Handle GET request here
+});
+
+// Get user's blogs route
+app.get('/api/:userid/blogs', function (req, res) {
+  // Handle GET request here
+});
+
+// Add blog route
+app.post('/api/addblog', function (req, res) {
+  // Handle POST request here
+});
+
+// Add comment to blog route
+app.post('/api/blogs/:blogid/comments', function (req, res) {
+  // Handle POST request here
+});
+
+// Remove comment from blog route
+app.delete('/api/blogs/:blogid/comments', function (req, res) {
+  // Handle DELETE request here
+});
+
+// Start the server
+app.listen(8000, function () {
+  console.log('Server started on port 3000');
+});
