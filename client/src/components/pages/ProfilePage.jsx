6d9cc6a568d31fa50c5ae68a06f333749c314123
cherrieszzz <greenteacore@outlook.com>
2023-04-21 10:33:12 +0800

    重构资料页：增加个人博客列表和评论

--- client/src/components/pages/ProfilePage.jsx
+++ client/src/components/pages/ProfilePage.jsx
@@ -4,8 +4,53 @@ import Layout from '../layouts/Layout';
 import { Link } from 'react-router-dom';
 import instance from '../../services/axiosInit';
 
+const UserBlogs = ({userId}) => {
+    const [blogs, setBlogs] = useState([]);
+
+    useEffect(() => {
+        function fetchData() {
+            instance.get(`/api/users/${userId}/posts`).then((res) => {
+                setBlogs(res.data);
+            }).catch((err) => {
+                console.log(err);
+            })
+        }
+        fetchData();
+    },[]);
+
+    return (
+        <div>
+            我的博客:
+            {blogs && blogs.map((blog) => <div key={blog._id}>{blog.title} </div>)}
+        </div>
+    )
+}
+
+const UserComments = ({userId}) => {
+    const [comments, setComments] = useState([]);
+
+    useEffect(() => {
+        function fetchData() {
+            instance.get(`/api/users/${userId}/comments`).then((res) => {
+                setComments(res.data);
+            }).catch((err) => {
+                console.log(err);
+            })
+        }
+        fetchData();
+    },[]);
+
+    return (
+        <div>
+             我的评论：
+            {comments && comments.map((comment) => <div key={comment._id}>{comment.content}</div>)}
+        </div>
+    )
+}
+
 const ProfilePage = () => {
     const { loggedInUser, logout } = useContext(AuthContext);
+   
     console.log(loggedInUser);
 
     if (!loggedInUser) {
@@ -16,15 +61,13 @@ const ProfilePage = () => {
         )
     }
 
-    useEffect(() => {
-        instance.get('/api/post')
-    })
-
     return (
         <Layout>
             {/* <p>{loggedInUser.username}</p> */}
             欢迎！{loggedInUser.username} <button onClick={logout}>登出</button>
-
+            <UserBlogs userId={loggedInUser.id} />
+            <UserComments userId={loggedInUser.id} />
+           
         </Layout>
     )
 }
